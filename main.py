2626a8572f8fba4a0a850fe909d3dd049a79d081
v15hnuf6n1off <145260326+v15hnuf6n1off@users.noreply.github.com>
2024-02-05 11:35:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -110,11 +110,9 @@ def loopthread(message,otherss=False):
 # start command
 @app.on_message(filters.command(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-    app.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**, i am Link Bypasser Bot, just send me any supported links and i will you get you results.\nCheckout /help to Read More__",
+    app.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**,Hi👋 Dude, I can bypass several sites, just send me any supported links and i will you get you results.\nCheckout /help to Read More__",
     reply_markup=InlineKeyboardMarkup([
-        [ InlineKeyboardButton("🌐 Source Code", url="https://github.com/bipinkrish/Link-Bypasser-Bot")],
-        [ InlineKeyboardButton("Replit", url="https://replit.com/@bipinkrish/Link-Bypasser#app.py") ]]), 
-        reply_to_message_id=message.id)
+        [ InlineKeyboardButton("Join Here", url="https://t.me/Mr_V_bots")]
 
 
 # help command
@@ -137,7 +135,7 @@ def docthread(message):
     file = app.download_media(message)
     dlccont = open(file,"r").read()
     links = bypasser.getlinks(dlccont)
-    app.edit_message_text(message.chat.id, msg.id, f'__{links}__', disable_web_page_preview=True)
+    app.edit_message_text(message.chat.id, msg.id, f'Here is your Link\n__{links}__', disable_web_page_preview=True)
     remove(file)
 
 
